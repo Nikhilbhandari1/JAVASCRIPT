98b294b97780fa12345cf24c688cfb1f5015d07b
Nikhilbhandari1 <bhandarinikhil970@gmail.com>
2023-12-20 15:09:27 +0530

    TOPIC REVISION JAVASCRIPT ASSIGMENT

---
+++ notes.js
@@ -0,0 +1,120 @@
+
+// so this is simple assigment qustions that help you to reconginezed the basic of javascript [ PART ONE ] and also i uploaded easy to hard assigment two complete javascript basic [DAY ONE EASY ASSINGMENT] check this qustions!!!!
+
+
+/*
+
+***[ THIS IS IF ELSE CONDITION ]
+
+let dividend = 10
+let divisor = 5
+
+if (dividend != 10) {
+    console.log("iam right");
+}else{
+    console.log("iam not right");
+}
+
+***[ IF ELSE CONDITION]
+
+let isadmin = true
+
+if (isadmin) {
+    console.log("getting user details");
+}else{
+    console.log("plz login here");
+}
+
+
+
+***[ SWITICH , CONDITION ]
+
+        let grade = "a"
+        switch (grade) {
+            case "b":
+                console.log("grade b is gratest");
+                break;
+            case "a":
+            console.log("grade a is gratest");
+            break
+            default:
+                break;
+        
+
+*** [ TO FIND OUT WHO IS GREATER AND SMALLER ]
+
+let a =10
+let b = 20
+let c = 30
+
+if (a>b) {
+    console.log("a is bigger");
+}if (b<c) {
+    console.log("b is grater than c");
+}else{
+    console.log("this is right");
+}
+
+*** [ TO FIND OUT TYPE OF ANYTHING ]
+console.log(typeof 2n);
+console.log(typeof null);
+console.log(typeof []);
+console.log(typeof NaN);
+console.log(typeof '')
+
+*** [ FOR LOOP CONDITION IN SECOND FOR LOOP YOU FIND CONTINUE IT MEANS AFTER THEY NO 8 THEY NO SHOULD PRINT!!! BASIC THAT I SHARED HERE ]
+
+for (some = 10; some <=15; ){
+    some ++
+console.log(some);
+
+*** for (let index = 6; index <=11; index++ ){
+    if (index=== 8) {
+        continue
+    }
+    console.log(index);
+
+
+*** [ ADDING THEY INTEGEAR ]
+
+ let numberofchild = 100
+ let numberofadults = 170
+ let numberofseniors = 200
+
+ console.log(numberofchild+numberofadults+numberofseniors);
+
+ *** [ TO FIND TYPEOF ANY VARIABLES ]
+ 
+ let name = 'nikhil'
+ let email = 'bhandarinikhil@gmail.com'
+ let age = 21
+
+ console.log(typeof age);
+
+*** [ FOR LOOP FOR BETTER UNDERSTANDING ]
+ 
+for (let index = 0; index <= 10; index++){
+console.log(index);
+}
+
+*** [ ARR IN THIS QUASTION PRINT ONLY POSITIVE NO NOT TO PRINT NEGATIVE NO SOLUTION IN THIS CODE ]
+
+let arr = [  -1 , -2 , 3 , -5 , 9, 8, -7, 7 ];
+ 
+for (let i = 0; i<arr.length; i++){
+if (arr[i]<5){
+    continue;
+}
+  console.log(arr[i]);
+}
+ 
+*** [ SIMPLE CALCULATOR TWO INR , AND USD EXCHANGE !!!]
+
+let amountInINR = 850;
+let amountInUSD = 850/82;
+console.log('${amountInINR} inr is${amountInUSD} usd');
+
+*/
+
+
+    console.log(" TQ TWODAY DAY ONE EASY JAVASCRIPT ASSIGMENT");
